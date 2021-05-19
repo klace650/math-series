46507c0ff24c56df655faef749d024ffa568ed85
Klace Koch <koch.klace@gmail.com>
2021-05-18 17:55:09 -0700

    forgot to switch branches

---
+++ tests/test_series.py
@@ -0,0 +1,44 @@
+from math_series.series import fibonacci
+
+def test_connect():
+  assert fibonacci
+
+def test_one():
+  actual = fibonacci(1)
+  expected = 1
+  assert actual == expected
+
+def test_two():
+  actual = fibonacci(2)
+  expected = 2
+  assert actual == expected
+
+# def test_three():
+#   actual = fibonacci(3)
+#   expected = 3
+#   assert actual == expected
+
+# def test_four():
+#   actual = fibonacci(4)
+#   expected = 4
+#   assert actual == expected
+
+# def test_five():
+#   actual = fibonacci(5)
+#   expected = 5
+#   assert actual == expected
+
+# def test_six():
+#   actual = fibonacci(6)
+#   expected = 6
+#   assert actual == expected
+
+# def test_seven():
+#   actual = fibonacci(7)
+#   expected = 7
+#   assert actual == expected
+
+# def test_eight():
+#   actual = fibonacci(8)
+#   expected = 8
+#   assert actual == expected
